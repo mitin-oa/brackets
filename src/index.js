25e195d3917703107b20a028cd2da5a6b7fcb04b
mitin_oleg <mitin_oa@mail.ru>
2023-01-28 16:32:22 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+  let sequence = [];
+  for (let bracket of str) {
+      let bracketsIndex = brackets.indexOf(bracket)
+      if (bracketsIndex === -1) {
+        continue
+      }
+      if (bracketsIndex % 2 === 0) {
+            sequence.push(bracketsIndex + 1)
+      } else {
+          if (sequence.pop() !== bracketsConfig) {
+            return false;
+          };
+        } 
+    }
+    return sequence.length === 0
 }
